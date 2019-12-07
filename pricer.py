ee035cc23fc1cc21a178a620e07cba8c30580e8e
Serozha <sergeylavrentyev8@gmail.com>
2019-12-07 11:02:02 +0300

    Solved problem of the same performace of async and sync url requests. Added bench_doc.txt file for documenting some benchmarks.

--- pricer.py
+++ pricer.py
@@ -4,30 +4,40 @@ import re
 import requests
 from requests import ConnectionError
 
-from aiohttp import ClientOSError
+from aiohttp import ClientOSError, ServerDisconnectedError
 import aiohttp
 from bs4 import BeautifulSoup
 from googlesearch import search
+from links_for_debug import links_dbg
 
+ASYNC = True
 
 item = 'Йогуртин'
+shops = {}
 
 
 async def get_shop_content(client, link):
-    async with client.get(link) as resp:
-        # assert resp.status == 200
-        page = await resp.text()
-        return page
+    try:
+        async with client.get(link) as resp:
+            page = await resp.text()
+            shops[link] = page
+            return
+    except ClientOSError:
+        print('fail to get', link)
+    except ServerDisconnectedError:
+        print('server disconnected', link)
+    except UnicodeDecodeError:
+        print('fail to decode unicode', link)
+    except BaseException:
+        print('got unknown exception from', link)
 
 
 def get_shops_list(item):
     links = []
-    for url in search(item, stop=10):
+    for url in search(item, num=100, stop=200):
         links.append(url)
+    print(links)
     return links
-
-
-shops = {}
 
 
 def sync_get_page(link):
@@ -37,21 +47,26 @@ def sync_get_page(link):
 
 async def main():
     google_time_start = time()
-    shop_links = get_shops_list(item)
+    shop_links = links_dbg   #links_dbg   #get_shops_list(item)
     print('time for googling', time() - google_time_start)
 
-    # for shop in shop_links:
-    #     try:
-    #         shops[shop] = sync_get_page(shop)
-    #     except ConnectionError:
-    #         print('fail to get ', shop)
-
-    # async with aiohttp.ClientSession() as client:
-    #     for shop in shop_links:
-    #         try:
-    #             shops[shop] = await get_shop_content(client, shop)
-    #         except ClientOSError:
-    #             print('fail to get', shop)
+    if not ASYNC:
+        print('SYNC')
+        for shop in shop_links:
+            try:
+                shops[shop] = sync_get_page(shop)
+            except ConnectionError:
+                print('fail to get ', shop)
+            except UnicodeDecodeError:
+                print('fail to decode unicode', shop)
+    else:
+        print('ASYNC')
+        tasks = []
+        async with aiohttp.ClientSession() as client:
+            for shop in shop_links:
+                task = asyncio.ensure_future(get_shop_content(client, shop))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
 
 
 def has_item(tag):
@@ -63,19 +78,20 @@ def has_item(tag):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     start_time = time()
-    loop.run_until_complete(main())
+    future = asyncio.ensure_future(main())
+    loop.run_until_complete(future)
 
-    # for link, html in shops.items():
-    #     if 'co2' in link:
-    #         soup = BeautifulSoup(html, features='html.parser')
-    #         print(link)
-    #         tags = soup.find_all(name='h1',
-    #                              string=re.compile(item))
-    #         if len(tags) != 0:
-    #             parent = tags[0].parent.parent
-    #             print(parent)
-    #         else:
-    #             print('not found item')
+    for link, html in shops.items():
+        soup = BeautifulSoup(html, features='html.parser')
+
+        tags = soup.find_all(name='h1',
+                             string=re.compile(item))
+        if len(tags) != 0:
+            print('item found', link)
+            # parent = tags[0].parent.parent
+            # print(parent)
+        else:
+            print('not found item')
 
     taken_time = time() - start_time
     print('TIME LEFT:', taken_time)
